d0986e52fe9480a8b43d7581ae6c1aa224bf9d9f
Naveen1612-svg <naveenak129@gmail.com>
2022-12-15 19:11:38 +0530

    moved git

--- Projectgit/src/org/githup/GitHup.java
+++ Projectgit/src/org/githup/GitHup.java
@@ -4,7 +4,7 @@ public class GitHup {
     public static void main(String[] args) {
 		System.out.println("this");
 		System.out.println("is");
-		System.out.println("thamarai");
+		System.out.println("naveen");
 		System.out.println("project");
 	}
 }
